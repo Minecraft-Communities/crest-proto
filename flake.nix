ad348b12a04e7c5ba5c46829d85d2e56687678be
Maxine Alexander <25956545+MaxTheMooshroom@users.noreply.github.com>
2024-09-12 12:11:28 -0400

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          packages = with pkgs; [
+            tree
+          ];
+
+          buildInputs = [
+            cargo
+            rustc
+            rustfmt
+            pre-commit
+            pkg-config
+
+            gobject-introspection
+            atkmm
+            pango
+            gtk3-x11
+
+            netsurf.libparserutils
+            netsurf.libwapcaplet
+          ];
+          LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${
+            lib.makeLibraryPath [
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXi
+              libxkbcommon
+              xorg.libxcb
+              vulkan-loader
+            ]
+          }";
+
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      }
+    );
+}
